e7495b0e50ad2a86b46eb42c50a2d886f7b5b5d3
ilirosmanaj <ilirosmanaj.94@gmail.com>
2019-11-14 13:50:11 +0100

    Only use class categories for oenace

--- src/filter_oeance.py
+++ src/filter_oeance.py
@@ -1,14 +1,23 @@
 import csv
 
+PATH_TO_RAW = '../data/raw/oenace2008.csv'
+PATH_TO_PREPROCESSED = '../data/preprocessed/oenace2008.csv'
+
 
 def main():
     """
     Filters out some records from the OENACE file, by selecting only products with
-    four digits in the code
+    four digits in the code (marked as level 4). The clean oeance file should endup with only 615 rows.
     """
-    # XXX: for now, we will try to map all the products, if possible. If that does not work out well, we will implement
-    # this filtering to only some categories
-    pass
+    preprocessed = []
+    with open(PATH_TO_RAW, 'r') as csv_file:
+        csv_reader = csv.DictReader(csv_file)
+        preprocessed = [row for row in csv_reader if row['Level'] == '4']
+
+    with open(PATH_TO_PREPROCESSED, 'w') as f:
+        w = csv.DictWriter(f, preprocessed[0].keys())
+        w.writeheader()
+        w.writerows(preprocessed)
 
 
 if __name__ == '__main__':
